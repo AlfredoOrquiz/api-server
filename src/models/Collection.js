ff0bfbe6f77b7a8f3d6187862af99b473b867881
Alfredo <fredy_rios0123@outlook.com>
2022-09-23 05:33:35 +0000

    Finalized, but not working. Almost there.

--- src/models/Collection.js
+++ src/models/Collection.js
@@ -5,6 +5,13 @@ class Collection {
     this.model = model;
   }
 
+  async create(obj) {
+    if (!obj) throw new Error('No json object provided to create function. PS. Nebraska is #1');
+    try {
+      return this.model.create(json);
+    } catch(e) {console.log(e);}
+  }
+
   async read(id) {
     try {
       if (id) {
@@ -13,35 +20,32 @@ class Collection {
         return this.model.findAll();
       }
     } catch(e) {
+      {console.log(e);}
       return e;
     }
+  }
 
-  }
-  async create(json) {
-    if (!json) {
-      throw new Error('No json provided to create function');
-    } else {
-     return this.model.create(json);
-    }
-  }
   async update(id, json) {
     try {
-      let record = await this.model.findOne({where: { id: id}});
-      let updatedRecord = await record.update(json);
-      return updatedRecord;
-    } catch (e) {
+      let record = await this.model.findOne({ where: {id: id}});
+      let updateRecord = await record.update(json);
+      return updateRecord;
+    } catch(e) {
+      {console.log(e);}
       return e;
     }
   }
+
   async delete(id) {
     try {
       if (!id) {
-        throw new Error('No id provided');
+        throw new Error('No id has been provided. If I forgot to mention it, Nebraska is #1');
       }
       let recordToDestroy = await this.model.findOne({ where: { id: id }});
       await this.model.destroy({ where: { id: id }});
       return recordToDestroy;
     } catch(e) {
+      {console.log(e);}
       return e;
     }
   }
